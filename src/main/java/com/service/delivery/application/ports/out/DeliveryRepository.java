28ac111130f59058772cb462527062086312b7d5
rabiebe <rabiebelkaida@gmail.com>
2025-02-14 22:01:12 +0100

    feat: implement DeliveryRepositoryImpl using JpaDeliveryRepository and DeliveryMapper

--- src/main/java/com/service/delivery/application/ports/out/DeliveryRepository.java
+++ src/main/java/com/service/delivery/application/ports/out/DeliveryRepository.java
@@ -1,6 +1,16 @@
 package com.service.delivery.application.ports.out;
 
 
+import com.service.delivery.domain.model.Delivery;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Stream;
+
 public interface DeliveryRepository  {
 
+    Delivery save(Delivery delivery);
+    Optional<Delivery> findById(UUID id);
+    List<Delivery> findAll();
 }
--- src/main/java/com/service/delivery/infrastructure/repository/DeliveryRepositoryImpl.java
+++ src/main/java/com/service/delivery/infrastructure/repository/DeliveryRepositoryImpl.java
@@ -2,18 +2,37 @@ package com.service.delivery.infrastructure.repository;
 
 import com.service.delivery.application.ports.out.DeliveryRepository;
 import com.service.delivery.domain.model.Delivery;
+import com.service.delivery.infrastructure.entity.DeliveryEntity;
+import com.service.delivery.infrastructure.mapper.DeliveryMapper;
 import com.service.delivery.infrastructure.repository.jpa.JpaDeliveryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 @RequiredArgsConstructor
 public class DeliveryRepositoryImpl implements DeliveryRepository {
 
     private final JpaDeliveryRepository deliveryJpaRepository;
+    private final DeliveryMapper deliveryMapper;
 
-//    void save(final Delivery delivery) {
-//
-//        deliveryJpaRepository.save()
-//    }
+    @Override
+    public Delivery save(Delivery delivery) {
+        DeliveryEntity entity = deliveryMapper.toEntity(delivery);
+        return deliveryMapper.toDomain(deliveryJpaRepository.save(entity));
+    }
+
+    @Override
+    public Optional<Delivery> findById(UUID id) {
+        return deliveryJpaRepository.findById(id).map(deliveryMapper::toDomain);
+    }
+
+    @Override
+    public List<Delivery> findAll() {
+        return deliveryJpaRepository.findAll().stream().map(deliveryMapper::toDomain).toList();
+    }
 }
+
